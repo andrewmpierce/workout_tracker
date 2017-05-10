1a562220fafe83a2ce2beb096412b4aa66de1e7b
Andrew Pierce <apierce1992@gmail.com>
2017-05-10 08:58:02 -0400

    added in form for submission

--- public/index.js
+++ public/index.js
@@ -1,10 +1,8 @@
-console.log("HELLLOOOO");
 $(document).ready(function(){
 
   $('.workout').click(function(e){
     console.log(this.id);
     $('#' + this.id + '_details').toggle();
-
   });
 
 });

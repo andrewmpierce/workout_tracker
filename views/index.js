91551078aa718af3ca9756b45dc0adb6af9ed39f
Andrew Pierce <apierce1992@gmail.com>
2017-06-06 21:04:16 -0400

    capitalized all exercise names

--- views/index.js
+++ views/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+  function capitalize (phrase) {
+    var words = phrase.split('_');
+    console.log(words);
+    var capitalized = [];
+    for (var word in words) {
+      console.log(words);
+      capitalized.push(words[word][0].toUpperCase() + words[word].slice(1));
+    }
+    return capitalized.join(' ');
+  }
+
   $(':button').click(function(e){
     console.log(this.id);
     $('#' + this.id + '_details').toggle();
@@ -8,8 +19,9 @@ $(document).ready(function(){
       console.log(data);
       for (var key in data) {
         var movement = key.slice(0, -7);
-        var movement_spaces = movement.replace(/_/g," ");
-        $('#' + movement ).text(movement_spaces + " - " + data[key] + " lbs ");
+        var capitalized_movement = capitalize(movement);
+        console.log(capitalized_movement);
+        $('#' + movement ).text(capitalized_movement + " - " + data[key] + " lbs ");
         $('[name=' + key + ']' ).val(data[key]);
 
       }
